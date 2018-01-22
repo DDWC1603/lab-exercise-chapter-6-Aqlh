e8e5aaa7e69e94102e267bf015371449075696e6
AqilahSyahirah_ <33884023+Aqlh@users.noreply.github.com>
2018-01-22 10:15:18 +0800

    task completed, thanks sir

--- String-Exercise-4.cpp
+++ String-Exercise-4.cpp
@@ -1,17 +1,21 @@
 //convert string to uppercase
 //complete the fragment code below using the string to uppercase function.
+//aqilah syahirah A17DW2330
 
 #include <iostream>
+#include<string.h>
+using namespace std;
 
 int main()
 {
 	char lowerToUpper[80];
+	int i;
 
-	/*use string copy function here*/(lowerToUpper, "This is a check");
+	strcpy(lowerToUpper, "This is a check");
 
 	for(i=0;i<80;i++)
 	{
-		lowerToUpper[i] = /*uppercase function*/(lowerToUpper[i]);
+		lowerToUpper[i] = toupper(lowerToUpper[i]);
 
 	}
 
