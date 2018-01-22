68f9645f8aff2bcc207138dca08254613c03ff91
AqilahSyahirah_ <33884023+Aqlh@users.noreply.github.com>
2018-01-22 10:27:38 +0800

    task completed, thanks sir

--- Array-Exercise-1.cpp
+++ Array-Exercise-1.cpp
@@ -1,20 +1,19 @@
 //its just an array example
 //find the sum of all the value inside the array
+//aqilah syahirah A17DW2330
 #include <iostream>
 using namespace std;
 
-ary[]={/*insert your own value*/};
-
-n, result=0;
 
 int main()
 {
+int ary[5]={5,34,56,2,5};
 
-	for(/* n is not more than 10*/)
+int n, result=0;
+
+	for(n=0;n<5;++n)
 	{
-		result += ary[];
+		result += ary[n];
 	}
 	cout<<result;
-	
-
 }
