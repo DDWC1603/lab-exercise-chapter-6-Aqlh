6d754f02bae1eda83511bd4b026a20f13251a85a
AqilahSyahirah_ <33884023+Aqlh@users.noreply.github.com>
2018-01-22 20:27:24 +0800

    task complete, thanks sir

--- Array-Exercise-5.cpp
+++ Array-Exercise-5.cpp
@@ -1,18 +1,21 @@
 //using the random function, the program make it own random number and find the maximum random number and print it out.
 //finish this program up
 //thank you.
+//aqilah syahirah A17DW2330
 
 #include <iostream>
+#include<cstdlib>
+using namespace std;
 int main()
 {
-	i, max=0;
-	list[100];
+	int i, max=0;
+	int list[100];
 
 	//initialize the array with random values
-	for(/*i<100*/) list[i] = /*insert the random function here*/;
+	for(i=0;i<100;i++) list[i] = rand();
 
 	//find the maximum val
-	for(/*i>100*/)
+	for(i=0;i>100;i++)
 		if(max<list[i]) max=list[i];
 
 	cout<<"Maximum value: "<<max;
